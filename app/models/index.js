abf63caab3f64ef9c1eac3b158218713020adbf4
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2022-07-18 11:43:54 +0700

    add dialect options

--- app/models/index.js
+++ app/models/index.js
@@ -4,14 +4,20 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   operatorsAliases: false,
 
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
