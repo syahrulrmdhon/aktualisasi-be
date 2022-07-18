8ad7e74655a740364bcf8cca94b72842c269302a
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2022-07-18 11:25:31 +0700

    add credential heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "123",
-  DB: "aktualisasi_db",
+  HOST: "ec2-52-205-61-230.compute-1.amazonaws.com",
+  USER: "dyryvmbdbjfbbz",
+  PASSWORD: "7589c8833ba4208dc6dac2b7b7ffbfe1cc4354d1e6e8baef50435727dd8fae9d",
+  DB: "dc657jhhp2ac5e",
   dialect: "postgres",
   pool: {
     max: 5,
