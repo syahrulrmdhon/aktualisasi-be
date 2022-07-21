c6e9d8eac8ffa0ac6d1e33e7a6de3a13e21b0beb
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2022-07-21 09:48:09 +0700

    add db config local

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,9 +4,9 @@ module.exports = {
   PASSWORD: "7589c8833ba4208dc6dac2b7b7ffbfe1cc4354d1e6e8baef50435727dd8fae9d",
   DB: "dc657jhhp2ac5e",
   dialect: "postgres",
-  ssl: true, 
+  ssl: true,
   dialectOptions: {
-    ssl: true
+    ssl: true,
   },
   pool: {
     max: 5,
@@ -15,3 +15,17 @@ module.exports = {
     idle: 10000,
   },
 };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "postgres",
+//   PASSWORD: "123",
+//   DB: "aktualisasi_db",
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
